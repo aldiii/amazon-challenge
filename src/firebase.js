daaeaad009f76cc06cdd48b1f72fafca6a511ce7
Aldona Tomzik <a.tomzik@gmail.com>
2020-09-09 00:17:05 +0200

    Added material-ui and material-ui icons added header

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBOGvYxodBZJdRwgPQ5p1ezl_VPkeerqqc",
+    authDomain: "e-challenge-9828b.firebaseapp.com",
+    databaseURL: "https://e-challenge-9828b.firebaseio.com",
+    projectId: "e-challenge-9828b",
+    storageBucket: "e-challenge-9828b.appspot.com",
+    messagingSenderId: "586157237824",
+    appId: "1:586157237824:web:a931cd45216aacb753a1a5"
+  };
