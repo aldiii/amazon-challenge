5433714f4498060f3df42c014aceac5c99ad40e0
Aldona Tomzik <a.tomzik@gmail.com>
2020-09-10 00:15:10 +0200

    Added checkoutProduct component

--- src/Checkout.js
+++ src/Checkout.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import "./Checkout.css"
 import Subtotal from "./Subtotal";
+import { useStateValue } from "./StateProvider";
+import CheckoutProduct from "./CheckoutProduct";
 
 function Checkout() {
+    const [{basket}, dispatch] = useStateValue(); 
+
     return (
         <div className="checkout">
             <div className="checkout__left">
@@ -11,6 +15,16 @@ function Checkout() {
                     <h2 className="checkout__title">
                         Your shopping basket
                     </h2>
+
+                    {basket.map(({id, image, title, price, rating}) =>(
+                        <CheckoutProduct 
+                        id={id}
+                        title={title}
+                        price={price}
+                        image={image}
+                        rating={rating}
+                    />
+                    ))}
                 </div>
             </div>
             <div className="checkout__right">
--- src/Subtotal.js
+++ src/Subtotal.js
@@ -2,14 +2,15 @@ import React from "react";
 import "./Subtotal.css";
 import CurrencyFormat from "react-currency-format";
 import { useStateValue } from "./StateProvider";
+import {getBasketTotal} from "./reducer";
 
 function Subtotal() {
   const [{ basket }, dispatch] = useStateValue();
 
-  const getBasketTotal = (basket) =>
+ /* const getBasketTotal = (basket) =>
     basket.reduce((prev, next) => {
       return prev + next.price;
-    }, 0);
+    }, 0); */
 
   return (
     <div className="subtotal">
@@ -17,7 +18,7 @@ function Subtotal() {
         renderText={(value) => (
           <>
             <p>
-              Subtotal ({basket.length} items) : <strong>{value}</strong>
+              Subtotal ({basket.length} items): <strong>{value}</strong>
             </p>
             <small className="subtotal__gift">
               <input type="checkbox" />
