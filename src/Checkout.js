5d6b02a0f8b56e12ef3705ebd79a05567691d04b
Aldona Tomzik <a.tomzik@gmail.com>
2020-09-09 20:02:37 +0200

    Added Subtotal component and styling react-currency fromat and contextApi

--- src/Checkout.js
+++ src/Checkout.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import "./Checkout.css"
+import Subtotal from "./Subtotal";
 
 function Checkout() {
     return (
         <div className="checkout">
-            <h1>Checkout</h1>
+            <div className="checkout__left">
+                <img src="https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423492668_.jpg" alt="" className="checkout__ad"/>
+                <div>
+                    <h2 className="checkout__title">
+                        Your shopping basket
+                    </h2>
+                </div>
+            </div>
+            <div className="checkout__right">
+                <Subtotal />
+            </div>
         </div>
     )
 }
--- src/Product.js
+++ src/Product.js
@@ -1,29 +1,46 @@
-import React from 'react'
-import "./Product.css"
+import React from "react";
+import "./Product.css";
+import { useStateValue } from "./StateProvider";
 
-function Product({title, image, price, rating}) {
-    return (
-        <div className="product">
-            <div className="product__info">
-                <p>{title}</p>
-                <p className="product__price">
-                    <small>$</small>
-                    <strong>{price}</strong>
-                </p>
-                <div className="product__rating">
-                    {Array(rating)
-                    .fill()
-                    .map((_,i)=>(
-                        <span role="img" aria-label="star">⭐</span>
-                    ))}                    
-                </div>
-            </div>
-            <img src={image} alt="lean startup" className="product__image"/>
-            <button className="product__button">
-                Add to basket
-            </button>
+function Product({ id, title, image, price, rating }) {
+  const [state, dispatch] = useStateValue();
+
+  const addToBasket = () => {
+    //dispach the item into the data layer
+    dispatch({
+      type: "ADD_TO_BASKET",
+      item: {
+        id: id,
+        title: title,
+        price: price,
+        rating: rating,
+      },
+    });
+  };
+  return (
+    <div className="product">
+      <div className="product__info">
+        <p>{title}</p>
+        <p className="product__price">
+          <small>$</small>
+          <strong>{price}</strong>
+        </p>
+        <div className="product__rating">
+          {Array(rating)
+            .fill()
+            .map((_, i) => (
+              <span role="img" aria-label="star">
+                ⭐
+              </span>
+            ))}
         </div>
-    )
+      </div>
+      <img src={image} alt="lean startup" className="product__image" />
+      <button className="product__button" onClick={addToBasket}>
+        Add to basket
+      </button>
+    </div>
+  );
 }
 
-export default Product
+export default Product;
